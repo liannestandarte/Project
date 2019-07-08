403893fa893dd3dcc1c11bfd97623315337b5af8
Norma <2015081633@ust-ics.mygbiz.com>
2019-07-08 21:09:38 +0800

    I used Galaxy Nexus API 24 as my Virtual Device Galaxy_Nexus_API_24

--- app/src/main/java/com/example/project/ProfileActivity.java
+++ app/src/main/java/com/example/project/ProfileActivity.java
@@ -1,18 +1,16 @@
 package com.example.project;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import android.content.Intent;
+        import android.content.SharedPreferences;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    TextView name, email;
     SharedPreferences pref;
-    String name_txt, email_txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +18,9 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         pref = getSharedPreferences("AppPref", MODE_PRIVATE);
-        name_txt = pref.getString("name", "");
-        email_txt = pref.getString("email", "");
 
         Button logout = (Button) findViewById(R.id.btnLogout);
         Button back = (Button) findViewById(R.id.btnBack);
-        name = (TextView) findViewById(R.id.name);
-        email = (TextView) findViewById(R.id.email);
-
-        name.setText(name_txt);
-        email.setText(email_txt);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,7 +29,7 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
-        logout.setOnClickListener(new View.OnClickListener() {
+            logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 SharedPreferences.Editor edit = pref.edit();
