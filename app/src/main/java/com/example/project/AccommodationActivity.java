ac33601c0b45d044e8a77fafe76bbf0d53e26c5e
Norma <2015081633@ust-ics.mygbiz.com>
2019-07-17 20:42:44 +0800

    I used nexus galaxy api 24

--- app/src/main/java/com/example/project/AccommodationActivity.java
+++ app/src/main/java/com/example/project/AccommodationActivity.java
@@ -2,11 +2,15 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class AccommodationActivity extends AppCompatActivity {
 
@@ -19,8 +23,44 @@ public class AccommodationActivity extends AppCompatActivity {
 
         pref = getSharedPreferences("AppPref", MODE_PRIVATE);
 
-        Button logout = (Button) findViewById(R.id.btnLogout);
+        final ImageButton deluxe = (ImageButton) findViewById(R.id.deluxeimgbtn);
+        final ImageButton one = (ImageButton) findViewById(R.id.onesuiteimgbtn);
+        final ImageButton two = (ImageButton) findViewById(R.id.twosuiteimgbtn);
+        final ImageButton three = (ImageButton) findViewById(R.id.threesuiteimgbtn);
         Button back = (Button) findViewById(R.id.btnBack);
+        Button logout = (Button) findViewById(R.id.btnLogout);
+
+        deluxe.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Deluxe Room reserved.", Toast.LENGTH_SHORT);
+                toast.show();
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+            }
+        });
+
+        one.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "One Bedroom Suite reserved.", Toast.LENGTH_SHORT);
+                toast.show();
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+            }
+        });
+
+        two.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Two Bedroom Suite reserved.", Toast.LENGTH_SHORT);
+                toast.show();
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+            }
+        });
+
+        three.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Three Bedroom Suite reserved.", Toast.LENGTH_SHORT);
+                toast.show();
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+            }
+        });
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
